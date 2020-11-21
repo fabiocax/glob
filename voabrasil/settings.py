75187bb9f27da8adddd510b4ddef2b135e2c5023
Fábio Alberti <fabiocax@gmail.com>
2020-11-21 08:00:55 -0300

    alter sqlite

--- voabrasil/settings.py
+++ voabrasil/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'voabrasil.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'blog/static/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'blog/static/imagens/db.sqlite3'),
     }
 }
 
